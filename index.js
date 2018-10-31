a8dac046b73936abeeb8ac4e9766dcd4161c04c8
ikiselev <ikiselev1989@gmail.com>
2018-10-31 14:45:44 +0300

    ver 3.6.1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  * Project:
  *      https://github.com/ikiselev1989/scrollmagic-image-sequencer-plugin
  *
- * Version: 3.6.0
+ * Version: 3.6.1
  *
  * Based on http://github.com/ertdfgcvb/Sequencer
  */
